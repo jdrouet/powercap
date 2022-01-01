b6ccdeab76f35937a99228c56e7637205c9b4362
Jérémie Drouet <jdrouet@users.noreply.github.com>
2022-01-01 19:32:24 +0100

    fix: total_energy takes also domain energy in account (#5)
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

--- src/mock.rs
+++ src/mock.rs
@@ -2,9 +2,24 @@ use std::fs;
 use std::io::Result;
 use std::path::Path;
 
+fn default_socket_generator(_socket: usize) -> u64 {
+    1234
+}
+
+fn default_domain_generator(_socket: usize, _domain: usize) -> u64 {
+    1234
+}
+
+type SocketEnergyGenerator = Box<dyn Fn(usize) -> u64>;
+type DomainEnergyGenerator = Box<dyn Fn(usize, usize) -> u64>;
+
 pub struct MockBuilder {
     enabled: bool,
     sockets: usize,
+    socket_energy_generator: SocketEnergyGenerator,
+    socket_max_energy_range_generator: SocketEnergyGenerator,
+    domain_energy_generator: Box<dyn Fn(usize, usize) -> u64>,
+    domain_max_energy_range_generator: Box<dyn Fn(usize, usize) -> u64>,
 }
 
 impl Default for MockBuilder {
@@ -12,17 +27,57 @@ impl Default for MockBuilder {
         Self {
             enabled: true,
             sockets: 1,
+            socket_energy_generator: Box::new(default_socket_generator),
+            socket_max_energy_range_generator: Box::new(default_socket_generator),
+            domain_energy_generator: Box::new(default_domain_generator),
+            domain_max_energy_range_generator: Box::new(default_domain_generator),
         }
     }
 }
 
 impl MockBuilder {
-    fn build_domain(&self, path: &Path, _socket: usize, _domain: usize, name: &str) -> Result<()> {
+    pub fn with_enabled(mut self, value: bool) -> Self {
+        self.enabled = value;
+        self
+    }
+
+    pub fn with_sockets(mut self, value: usize) -> Self {
+        self.sockets = value;
+        self
+    }
+
+    pub fn with_socket_energy_generator(mut self, value: SocketEnergyGenerator) -> Self {
+        self.socket_energy_generator = value;
+        self
+    }
+
+    pub fn with_socket_max_energy_range_generator(mut self, value: SocketEnergyGenerator) -> Self {
+        self.socket_max_energy_range_generator = value;
+        self
+    }
+
+    pub fn with_domain_energy_generator(mut self, value: DomainEnergyGenerator) -> Self {
+        self.domain_energy_generator = value;
+        self
+    }
+
+    pub fn with_domain_max_energy_range_generator(mut self, value: DomainEnergyGenerator) -> Self {
+        self.domain_max_energy_range_generator = value;
+        self
+    }
+
+    fn build_domain(&self, path: &Path, socket: usize, domain: usize, name: &str) -> Result<()> {
         fs::create_dir_all(&path)?;
         fs::write(&path.join("name"), name)?;
         fs::write(&path.join("enabled"), if self.enabled { "1" } else { "0" })?;
-        fs::write(&path.join("energy_uj"), "123456")?;
-        fs::write(&path.join("max_energy_range_uj"), "123456")?;
+        fs::write(
+            &path.join("energy_uj"),
+            (self.domain_energy_generator)(socket, domain).to_string(),
+        )?;
+        fs::write(
+            &path.join("max_energy_range_uj"),
+            (self.domain_max_energy_range_generator)(socket, domain).to_string(),
+        )?;
         Ok(())
     }
 
@@ -30,8 +85,14 @@ impl MockBuilder {
         fs::create_dir_all(&path)?;
         fs::write(&path.join("name"), format!("package-{}", socket))?;
         fs::write(&path.join("enabled"), if self.enabled { "1" } else { "0" })?;
-        fs::write(&path.join("energy_uj"), "123456")?;
-        fs::write(&path.join("max_energy_range_uj"), "123456")?;
+        fs::write(
+            &path.join("energy_uj"),
+            (self.socket_energy_generator)(socket).to_string(),
+        )?;
+        fs::write(
+            &path.join("max_energy_range_uj"),
+            (self.socket_max_energy_range_generator)(socket).to_string(),
+        )?;
         for (i, name) in ["core", "nocore", "dram"].iter().enumerate() {
             let domain_path = path.join(format!("intel-rapl:{}:{}", socket, i));
             self.build_domain(&domain_path, socket, i, name)?;
